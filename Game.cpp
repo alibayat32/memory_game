377496b2952659e94f0b228053f08970749a791e
Ali <ali1802bayat@gmail.com>
2023-06-10 14:16:53 -0700

    add function to get user input & validate it

--- Game.cpp
+++ Game.cpp
@@ -10,3 +10,20 @@ int get_random_digit() {
     return rand() % 8 + 2;
 }
 
+// Function to get user input and validate it
+int get_user_input() {
+    int input;
+    while (true) {
+        cin >> input;
+        if (cin.fail() || input < 2 || input > 9) {
+            cout << "Invalid input! Please enter a single digit number between 2 and 9." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Enter the length of digits again: ";
+        } else {
+            break;
+        }
+    }
+    return input;
+}
+
